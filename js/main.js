0ee1ce5614104c336af841bcb36d9dd628bad47c
Linda Ikechukwu <lindaedwin96@gmail.com>
2018-08-29 02:51:42 +0100

    modified the about tab

--- js/main.js
+++ js/main.js
@@ -16,23 +16,7 @@ function TypeWriter(){
 }
 TypeWriter();
 
-/* Remove = () => {
-    if(introTxt.length == txtLength){
-        intro.innerHTML = "";
-        setTimeout(Remove,5000);
-    }
-}
-Together =() =>{
-    TypeWriter();
-    Remove();
-    setTimeout(Together,7000);
-}
 
-
-
-if (Remove){
-    Together();
-}*/
   
 //Form Validation
 (function ($) {
@@ -91,3 +75,26 @@ if (Remove){
     
 
 })(jQuery);
+
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+      'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+      'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+      'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+      'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+  
+    while (length--) {
+      method = methods[length];
+  
+      // Only stub undefined methods.
+      if (!console[method]) {
+        console[method] = noop;
+      }
+    }
+  }());
